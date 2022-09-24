a0ce5e4f16c3dc441d102337fd12b2b7fa381817
Virgy <email@gmail.com>
2022-09-24 17:22:07 +0500

    add new .py product_page

--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,13 @@ class MainPageLocators():
 
 
 class LoginPageLocators():
-    
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BASKET_TOTAL_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_NAME_AFT_ADDED = (By.CSS_SELECTOR, ".alert-success strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    PRODUCT_TITLE = (By.CSS_SELECTOR, ".product_main h1")
---
+++ pages/product_page.py
@@ -0,0 +1,30 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_to_basket_btn.click()
+
+
+    def should_be_message_about_product_into_basket(self):
+        product_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
+        product_added_to_basket_alert = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_AFT_ADDED).text
+        assert product_title == product_added_to_basket_alert, f"Product {product_title} do not added to basket"
+
+
+    def should_be_message_basket_total(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_PRICE).text
+        assert product_price == basket_total, "Total amount does not match with added product price"
+
+    def should_be_disappear_message(self):
+        assert self.is_disappeared(*ProductPageLocators.PRODUCT_NAME_AFT_ADDED),"Success message! But should not to be"
+
+        
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.PRODUCT_NAME_AFT_ADDED),  "Success message! But shoul no to be"
+           
