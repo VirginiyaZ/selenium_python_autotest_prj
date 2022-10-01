41072e96d85723cfb720f79819914bc6abb707c4
Virgy <email@gmail.com>
2022-10-01 16:02:59 +0500

    add @pytest.mark.parametrize

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,12 +13,14 @@ class ProductPage(BasePage):
         product_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
         product_added_to_basket_alert = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_AFT_ADDED).text
         assert product_title == product_added_to_basket_alert, f"Product {product_title} do not added to basket"
-
+        # print(product_title)
 
     def should_be_message_basket_total(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_PRICE).text
+        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_PRICE).text  
         assert product_price == basket_total, "Total amount does not match with added product price"
+        # print(product_price)
+
 
     def should_be_disappear_message(self):
         assert self.is_disappeared(*ProductPageLocators.PRODUCT_NAME_AFT_ADDED),"Success message! But should not to be"
