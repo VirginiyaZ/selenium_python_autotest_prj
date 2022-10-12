00bef08482cff6cf780ad96708774ece34001fa9
Virgy <email@gmail.com>
2022-10-12 22:31:08 +0500

    check up commit

--- test_product_page.py
+++ test_product_page.py
@@ -18,44 +18,44 @@ import time
 #     page.should_be_message_basket_total()
 
 
-# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-#     page = ProductPage(browser,link)
-#     page.open()
-#     page.add_to_basket()
-#     page.should_not_be_success_message()
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
 
-# def test_guest_cant_see_success_message(browser):
-#     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-#     page = ProductPage(browser,link)
-#     page.open()
-#     page.should_not_be_success_message()
-
-
-# def test_message_disappeared_after_adding_product_to_basket(browser):
-#     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-#     page = ProductPage(browser,link)
-#     page.open()
-#     page.add_to_basket()
-#     time.sleep(1)
-#     page.should_be_disappear_message()
+def test_guest_cant_see_success_message(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser,link)
+    page.open()
+    page.should_not_be_success_message()
 
 
-# def test_guest_should_see_login_link_on_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser,link)
-#     page.open()
-#     page.should_be_login_link()
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_to_basket()
+    time.sleep(1)
+    page.should_be_disappear_message()
 
 
-# def test_guest_can_go_to_login_page_from_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     product_page = ProductPage(browser,link)
-#     login_page = LoginPage(browser,link)
-#     product_page.open()
-#     product_page.should_be_login_link()
-#     product_page.go_to_login_page()
-#     login_page.should_be_login_form()
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser,link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser,link)
+    login_page = LoginPage(browser,link)
+    product_page.open()
+    product_page.should_be_login_link()
+    product_page.go_to_login_page()
+    login_page.should_be_login_form()
 
 
 
