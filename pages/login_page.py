c9d670aadb9bdb0e7b86ac684e6554bfe50bc376
Virgy <email@gmail.com>
2022-10-22 19:46:46 +0500

    clear up files and mark with @pytest.mark.need_review

--- pages/login_page.py
+++ pages/login_page.py
@@ -22,17 +22,11 @@ class LoginPage(BasePage):
 
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        # assert True
         assert "login" in self.url, "URL not found"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        # assert True
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        # assert True
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -26,63 +26,73 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
-    
+
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self,browser):
         link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = ProductPage(browser,link)
         page.open()
         page.should_not_be_success_message()
         page.add_to_basket()
-        # page.solve_quiz_and_get_code()
         page.should_be_message_about_product_into_basket()
         page.should_be_message_basket_total()
 
 
-
-# @pytest.mark.xfail
-# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-#     page = ProductPage(browser,link)
-#     page.open()
-#     page.add_to_basket()
-#     page.should_not_be_success_message()
-
-
-# @pytest.mark.xfail
-# def test_message_disappeared_after_adding_product_to_basket(browser):
-#     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-#     page = ProductPage(browser,link)
-#     page.open()
-#     page.add_to_basket()
-#     time.sleep(1)
-#     page.should_be_disappear_message()
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_message_about_product_into_basket()
+    page.should_be_message_basket_total()
 
 
-# def test_guest_should_see_login_link_on_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser,link)
-#     page.open()
-#     page.should_be_login_link()
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_to_basket()
+    time.sleep(1)
+    page.should_be_disappear_message()
+
+@pytest.mark.skip
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser,link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser,link)
+    login_page = LoginPage(browser,link)
+    product_page.open()
+    product_page.should_be_login_link()
+    product_page.go_to_login_page()
+    login_page.should_be_login_form()
 
 
-# def test_guest_can_go_to_login_page_from_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     product_page = ProductPage(browser,link)
-#     login_page = LoginPage(browser,link)
-#     product_page.open()
-#     product_page.should_be_login_link()
-#     product_page.go_to_login_page()
-#     login_page.should_be_login_form()
-
-
-# @pytest.mark.page_review
-# def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = BasketPage(browser, link)
-#     page.open()
-#     page.basket_view()
-#     page.should_be_empty_basket()
-#     page.should_be_empty_basket_text()
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.basket_view()
+    page.should_be_empty_basket()
+    page.should_be_empty_basket_text()
 
     
     
